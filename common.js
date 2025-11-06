81f1964db879b95f0e656d45fed276fe017a3924
fujiwara-mu <fujiwart2@gmail.com>
2025-11-06 16:53:14 +0900

    Add files via upload
    ユーザーの指摘4

--- common.js
+++ common.js
@@ -535,9 +535,10 @@ document.addEventListener('DOMContentLoaded', () => {
             try {
                 const response = await fetch('https://script.google.com/macros/s/AKfycbxnLIUYN-XaatN_YcNAzHwYO6HPGqdFhjMJS2joS-sOL2Zx6QLyguWaGSGeo7AQT5_zUg/exec', {
                     method: 'POST',
-                    mode: 'cors', // CORSモードを明示的に指定
+                    mode: 'cors',
                     headers: {
-                        'Content-Type': 'application/json',
+                        // プリフライトリクエストを回避するため、Content-Typeをtext/plainに変更
+                        'Content-Type': 'text/plain',
                     },
                     body: JSON.stringify(data),
                 });
